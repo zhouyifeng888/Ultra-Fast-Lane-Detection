cee33206322092f7219dd6a0e6da7d15194850ae
zhouyifeng <984778782@qq.com>
2022-09-10 18:03:02 +0800

    sync

--- src/loss.py
+++ src/loss.py
@@ -1,52 +1,63 @@
 
+
+import numpy as np
+
 import mindspore.nn as nn
 import mindspore.ops as P
+from mindspore import Tensor
+
 
 class SoftmaxFocalLoss(nn.Cell):
-    def __init__(self, gamma, ignore_lb=255, num_lanes=4):
+    def __init__(self, gamma, num_lanes=4):
         super(SoftmaxFocalLoss, self).__init__()
         self.gamma = gamma
-        self.softmax=P.Softmax(axis=1)
-        self.nll = P.NLLLoss(ignore_index=ignore_lb)
+        self.softmax = P.Softmax(axis=1)
+        self.pow = P.Pow()
+        self.log_softmax = nn.LogSoftmax(axis=1)
+        self.weight = Tensor(np.ones((num_lanes,))).astype(np.float32)
+        self.nll = P.NLLLoss(reduction="mean")
 
     def construct(self, logits, labels):
         scores = self.softmax(logits)
-        factor = torch.pow(1.-scores, self.gamma)
-        log_score = F.log_softmax(logits, dim=1)
+        factor = self.pow(1.0 - scores, self.gamma)
+        log_score = self.log_softmax(logits)
         log_score = factor * log_score
-        loss = self.nll(log_score, labels)
+        loss = self.nll(log_score, labels, self.weight)
         return loss
+
 
 class ParsingRelationLoss(nn.Cell):
     def __init__(self):
         super(ParsingRelationLoss, self).__init__()
-    def construct(self,logits):
-        n,c,h,w = logits.shape
-        loss_all = []
-        for i in range(0,h-1):
-            loss_all.append(logits[:,:,i,:] - logits[:,:,i+1,:])
-        #loss0 : n,c,w
-        loss = torch.cat(loss_all)
-        return torch.nn.functional.smooth_l1_loss(loss,torch.zeros_like(loss))
 
+    def construct(self, logits):
+        n, c, h, w = logits.shape
+        loss_all = []
+        for i in range(0, h - 1):
+            loss_all.append(logits[:, :, i, :] - logits[:, :, i + 1, :])
+        # loss0 : n,c,w
+        loss = torch.cat(loss_all)
+        return torch.nn.functional.smooth_l1_loss(loss, torch.zeros_like(loss))
 
 
 class ParsingRelationDis(nn.Cell):
     def __init__(self):
         super(ParsingRelationDis, self).__init__()
         self.l1 = torch.nn.L1Loss()
+
     def construct(self, x):
-        n,dim,num_rows,num_cols = x.shape
-        x = torch.nn.functional.softmax(x[:,:dim-1,:,:],dim=1)
-        embedding = torch.Tensor(np.arange(dim-1)).float().to(x.device).view(1,-1,1,1)
-        pos = torch.sum(x*embedding,dim = 1)
+        n, dim, num_rows, num_cols = x.shape
+        x = torch.nn.functional.softmax(x[:, :dim - 1, :, :], dim=1)
+        embedding = torch.Tensor(
+            np.arange(dim - 1)).float().to(x.device).view(1, -1, 1, 1)
+        pos = torch.sum(x * embedding, dim=1)
 
         diff_list1 = []
-        for i in range(0,num_rows // 2):
-            diff_list1.append(pos[:,i,:] - pos[:,i+1,:])
+        for i in range(0, num_rows // 2):
+            diff_list1.append(pos[:, i, :] - pos[:, i + 1, :])
 
         loss = 0
-        for i in range(len(diff_list1)-1):
-            loss += self.l1(diff_list1[i],diff_list1[i+1])
+        for i in range(len(diff_list1) - 1):
+            loss += self.l1(diff_list1[i], diff_list1[i + 1])
         loss /= len(diff_list1) - 1
         return loss
