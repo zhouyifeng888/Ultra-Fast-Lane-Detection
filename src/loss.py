ac1b9f2ededd45aa36f9b7f1e9d121ab2c63f344
zhouyifeng <984778782@qq.com>
2022-09-11 18:30:48 +0800

    sync

--- src/loss.py
+++ src/loss.py
@@ -25,10 +25,7 @@ class SoftmaxFocalLoss(nn.Cell):
         factor = self.pow(1.0 - scores, self.gamma)
         log_score = self.log_softmax(logits)
         log_score = factor * log_score
-        print(f'log_score.shape :{log_score.shape}============')
-        print(f'labels.shape :{labels.shape}==============')
         loss = self.nll(log_score, labels, self.weight)
-        print(f'loss.shape :{loss.shape}======================')
         return loss
 
 
@@ -49,11 +46,10 @@ class ParsingRelationLoss(nn.Cell):
 
 
 class ParsingRelationDis(nn.Cell):
-    def __init__(self, griding_num=100, anchor_nums=56, num_lanes=4, data_type=ms.float16):
+    def __init__(self, griding_num=100, anchor_nums=56, data_type=ms.float16):
         super(ParsingRelationDis, self).__init__()
         self.dim = griding_num
         self.num_rows = anchor_nums
-        self.num_cols = num_lanes
 
         self.softmax = P.Softmax(axis=1)
         self.embedding = Tensor(
@@ -79,20 +75,32 @@ class ParsingRelationDis(nn.Cell):
 class TrainLoss(nn.Cell):
     def __init__(self, gamma=2, data_type=ms.float16):
         super(TrainLoss, self).__init__()
+        self.num_lanes = cfg.num_lanes
+        self.depth = cfg.griding_num + 1
+        self.onehot = P.OneHot(axis=1)
+        self.on_value = Tensor(1.0, ms.float32)
+        self.off_value = Tensor(0.0, ms.float32)
+        self.reshape = P.Reshape()
+
         self.w1 = 1.0
-        self.loss1 = SoftmaxFocalLoss(gamma=gamma, num_lanes=cfg.num_lanes)
+#        self.loss1 = SoftmaxFocalLoss(gamma=gamma, num_lanes=cfg.num_lanes)
+        self.loss1 = nn.FocalLoss(weight=None, gamma=2.0, reduction='mean')
         self.w2 = cfg.sim_loss_w
         self.loss2 = ParsingRelationLoss()
         self.w3 = 1.0
         self.loss3 = nn.SoftmaxCrossEntropyWithLogits(
-            sparse=False, reduction='mean')
+            sparse=True, reduction='mean')
         self.w4 = cfg.shp_loss_w
         self.loss4 = ParsingRelationDis(
-            griding_num=cfg.griding_num, anchor_nums=len(cfg.row_anchor), num_lanes=cfg.num_lanes, data_type=data_type)
+            griding_num=cfg.griding_num, anchor_nums=len(cfg.row_anchor), data_type=data_type)
 
     def construct(self, cls_out, seg_out, cls_label, seg_label):
-        total_loss = self.w1 * self.loss1(cls_out, cls_label) + self.w2 * self.loss2(
-            cls_out) + self.w3 * self.loss3(seg_out, seg_label) + self.w4 * self.loss4(cls_out)
+        cls_label = self.onehot(cls_label, self.depth,
+                                self.on_value, self.off_value)
+        total_loss = self.w1 * self.loss1(cls_out, cls_label) + \
+            self.w2 * self.loss2(cls_out) + \
+            self.w3 * self.loss3(self.reshape(seg_out, (-1, self.num_lanes + 1)), self.reshape(seg_label, (-1,))) + \
+            self.w4 * self.loss4(cls_out)
         return total_loss
 
 
