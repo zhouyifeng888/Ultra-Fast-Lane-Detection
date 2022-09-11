690daba8c496e1c10012c35cd85f2dc07c72a3f8
zhouyifeng <984778782@qq.com>
2022-09-12 01:12:38 +0800

    sync

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,9 @@
 
 import math
+
+import scipy
 import numpy as np
+from sklearn.linear_model import LinearRegression
 
 from mindspore.common.initializer import (Normal, One, Uniform, Zero)
 
@@ -34,3 +37,67 @@ def print_trainable_params_count(network):
         size = np.prod(shape)
         trainable_params_count += size
     print("trainable_params_count:" + str(trainable_params_count))
+
+
+class TusimpleAccEval(object):
+    lr = LinearRegression()
+    pixel_thresh = 20
+
+    @staticmethod
+    def generate_tusimple_lines(out, shape, griding_num, localization_type='rel'):
+
+        out = out.asnumpy()
+        out_loc = np.argmax(out, axis=0)
+
+        if localization_type == 'rel':
+            prob = scipy.special.softmax(out[:-1, :, :], axis=0)
+            idx = np.arange(griding_num)
+            idx = idx.reshape(-1, 1, 1)
+
+            loc = np.sum(prob * idx, axis=0)
+
+            loc[out_loc == griding_num] = griding_num
+            out_loc = loc
+        lanes = []
+        for i in range(out_loc.shape[1]):
+            out_i = out_loc[:, i]
+            lane = [int(round((loc + 0.5) * 1280.0 / (griding_num - 1)))
+                    if loc != griding_num else -2 for loc in out_i]
+            lanes.append(lane)
+        return lanes
+
+    @staticmethod
+    def line_accuracy(pred, gt, thresh):
+        pred = np.array([p if p >= 0 else -100 for p in pred])
+        gt = np.array([g if g >= 0 else -100 for g in gt])
+        return np.sum(np.where(np.abs(pred - gt) < thresh, 1., 0.)) / len(gt)
+
+    @staticmethod
+    def get_angle(xs, y_samples):
+        xs, ys = xs[xs >= 0], y_samples[xs >= 0]
+        if len(xs) > 1:
+            TusimpleAccEval.lr.fit(ys[:, None], xs)
+            k = TusimpleAccEval.lr.coef_[0]
+            theta = np.arctan(k)
+        else:
+            theta = 0
+        return theta
+
+    @staticmethod
+    def bench(pred, gt, y_samples, running_time):
+        if any(len(p) != len(y_samples) for p in pred):
+            raise Exception('Format of lanes error.')
+        angles = [TusimpleAccEval.get_angle(
+            np.array(x_gts), np.array(y_samples)) for x_gts in gt]
+        threshs = [TusimpleAccEval.pixel_thresh /
+                   np.cos(angle) for angle in angles]
+        line_accs = []
+        for x_gts, thresh in zip(gt, threshs):
+            accs = [TusimpleAccEval.line_accuracy(
+                np.array(x_preds), np.array(x_gts), thresh) for x_preds in pred]
+            max_acc = np.max(accs) if len(accs) > 0 else 0.
+            line_accs.append(max_acc)
+        s = sum(line_accs)
+        if len(gt) > 4:
+            s -= min(line_accs)
+        return s / max(min(4.0, len(gt)), 1.)
